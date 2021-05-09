ff604e0ee6f45ae61e79eef9f0d2ec97a8e00ca1
amit-mangaraj <78844720+amit-mangaraj@users.noreply.github.com>
2021-05-09 18:10:11 +0530

    first commit

---
+++ src/main/java/com/ltts/PoclogApplication.java
@@ -0,0 +1,13 @@
+package com.ltts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoclogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoclogApplication.class, args);
+	}
+
+}
